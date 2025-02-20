0d8ad88071d94d44c38c5b7152dc4bd24c7a124f
Christoffer-Robin Vaksdal <chris.vaksdal@glucoset.com>
2025-02-20 21:15:00 +0100

    Working on Exercise2.md

--- Exercise 2/Exercise2.md
+++ Exercise 2/Exercise2.md
@@ -1,18 +1,38 @@
-# Exercise 2 - Ultrasound Transmission and Reception
+# MEDT4165 - Exercise 2 Ultrasound Transmission and Reception
 
-## Part 1
+Written by: Christoffer-Robin Vaksdal
 
-### Generating a Gaussian-weighted sinusoidal pulse
+## Part 1 - Transmission
 
-A Gaussian-weighted sinusoidal pulse - A "Gaussian pulse".
+### Generating signals
+
+For this exercise we are starting out with generating two signals, a
+Gaussian-weighted sinusoidal pulse and a square wave pulse. Both signals have
+a frequency `f0 = 2.5MHz`. We can define these signals as `numpy.ndarray`s
+using `scipy`, starting with the gaussian pulse:
 
 ```python
-def generateGaussianPulse(A, f0, fs, T, sigma):
-    timeVector = np.arange(-T/2, T/2, 1/fs)
-    gauss = np.exp(-timeVector**2 / (2 * sigma**2))
-    sine = np.sin(2 * np.pi * f0 * timeVector)
-    return A * gauss * sine
+fs = 250 * 1e6      # Sample rate
+f0 = 2.5 * 1e6      # Signal base frequency
+T = 10 * 1e-6       # Length of time vector
+N = int(T * fs)     # Number of samples
+
+bw = 0.3 # Relative bandwidth. bw_abs = 75MHz
+
+timeVec = np.linspace(-T / 2, T / 2, N)
+
+gaussPulse, _imagGaussPulse, _envGaussPulse = signal.gausspulse(timeVec,
+                                                               fc=f0,
+                                                               bw=bw,
+                                                               retenv=True,
+                                                               retquad=True)
 ```
 
->> [task1_gauss](figures/task1_gauss.png")
->> Plot of gaussian pulse.
+So we define a time-vector, establish a sampling frequency and base frequency
+of the signal, as well as a relative bandwidth. The arguments `retenv` and
+`retquad` are for returning the signal envelope and imaginary part,
+respectively. Here's a plot of all three:
+
+>> ![task1_gauss_pulse](figures/task1_gauss_pulse.png)
+>> Plot of gaussian pulse. Along with the signal you can see the imaginary
+>> parts and the envelope of the signal.
--- Exercise 2/main.py
+++ Exercise 2/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import scipy.signal as signal
 import matplotlib.pyplot as plt
 
+from helpers import getOutputDir
+
 # def plot(axes, x, y, title:str|None=None, xLabel:str|None=None, yLabel:str|None=None, xLim:Tuple|None=None, yLim:Tuple|None=None, xticks:list[int]|None=None, yticks:list[int]|None=None):
 
 
@@ -22,17 +24,17 @@ def zeroPad(signal, N):
         return np.pad(signal, padWidth)
     return np.pad(signal, (padWidth, padWidth + 1))
 
+def savePlot(fig, name):
+    fig.savefig(f"{getOutputDir(2)}/figures/{name}")
 
 fs = 250 * 1e6
 f0 = 2.5 * 1e6
 T = 10 * 1e-6
 
 
-def task1_transmission():
+def task1_transmission(timeVec, f0, plot=False):
     bw = 0.3
     bw_abs = bw * f0
-
-    timeVec = np.linspace(-T / 2, T / 2, int(T * fs))
 
     # Gauss pulse:
     gausspulse, iGausspulse, envGaussPulse = signal.gausspulse(timeVec,
@@ -70,72 +72,196 @@ def task1_transmission():
     print(f"Pulse length: {pulseLengthN} samples")
     print(f"Pulse length: {pulseLengthMillimeters:.2f} mm")
 
-    # Plotting
+    if plot:
+        fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(30, 20))
+        axes.plot(timeVec * 1e6, gausspulse)
+        axes.plot(timeVec * 1e6, iGausspulse)
+        axes.plot(timeVec * 1e6, envGaussPulse)
+        axes.plot(timeVec * 1e6, signalEnv)
+        axes.legend(
+            ["Real part", "Imaginary Part", "Envelope", "Pulse Envelope"])
+        axes.set_title(f"Gauss Pulse (f0={f0*1e-6} MHz, bw={bw_abs*1e-6} MHz)")
 
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(30, 20))
-    axes.plot(timeVec * 1e6, gausspulse)
-    axes.plot(timeVec * 1e6, iGausspulse)
-    axes.plot(timeVec * 1e6, envGaussPulse)
-    axes.plot(timeVec * 1e6, signalEnv)
-    axes.legend(["Real part", "Imaginary Part", "Envelope", "Pulse Envelope"])
-    axes.set_title(f"Gauss Pulse (f0={f0*1e-6} MHz, bw={bw_abs*1e-6} MHz)")
+        savePlot(fig, "task1_gauss_pulse.png")
 
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(60, 40))
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(60, 40))
 
-    axes[0].plot(timeVec * 1e6, gausspulse)
-    axes[0].plot(timeVec * 1e6, paddedSquarePulse)
-    axes[0].set_title(f"Gauss Pulse vs. Square pulse")
-    axes[0].legend(["Gauss Pulse", "Square Pulse"])
+        axes[0].plot(timeVec * 1e6, gausspulse)
+        axes[0].plot(timeVec * 1e6, paddedSquarePulse)
+        axes[0].set_title(f"Gauss Pulse vs. Square pulse")
+        axes[0].legend(["Gauss Pulse", "Square Pulse"])
 
-    axes[1].plot(gaussFreqs * 1e-6, gaussSpectrum)
-    axes[1].plot(squareFreqs * 1e-6, squareSpectrum)
+        axes[1].plot(gaussFreqs * 1e-6, gaussSpectrum)
+        axes[1].plot(squareFreqs * 1e-6, squareSpectrum)
 
-    axes[1].set_xlim(-(f0 * 5 * 1e-6), (f0 * 5 * 1e-6))
-    # axes[1].set_ylim(-45, 10)
+        axes[1].set_xlim(-(f0 * 5 * 1e-6), (f0 * 5 * 1e-6))
+        # axes[1].set_ylim(-45, 10)
 
-    axes[1].set_title(f"Power Spectra (f0={f0*1e-6} MHz, T={1/f0*1e6:.2f} us)")
-    axes[1].legend(["Gauss Pulse", "Square Pulse"])
+        axes[1].set_title(
+            f"Power Spectra (f0={f0*1e-6} MHz, T={1/f0*1e6:.2f} us)")
+        axes[1].legend(["Gauss Pulse", "Square Pulse"])
 
-    return timeVec, gausspulse, paddedSquarePulse, gaussFreqs, gaussSpectrum, squareSpectrum
+    return gausspulse, paddedSquarePulse, gaussFreqs, gaussSpectrum, squareSpectrum
 
 
-def task1_transducer(timeVec, signals: list, freqs, spectra: list, names: list):
+def task1_transducer(
+                     fc,
+        timeVec,
+                     signals: list,
+                     freqs,
+                     spectra: list,
+                     names: list,
+                     plot=False):
     bw = 0.4
 
-    impulseResponse = signal.gausspulse(timeVec, fc=f0, bw=bw)
+    impulseResponse = signal.gausspulse(timeVec, fc=fc, bw=bw)
     freqResponse, transducerFreqs = powerSpectrum(impulseResponse, fs)
 
+    if plot:
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(60, 40))
+
+        axes[0].plot(timeVec * 1e6, impulseResponse)
+        for sig in signals:
+            axes[0].plot(timeVec * 1e6, sig)
+        axes[0].set_title(f"Signals")
+        axes[0].legend(["transducer"] + names)
+        axes[0].set_xlim(-T * 1e6 / 4, T * 1e6 / 4)
+
+        axes[1].plot(transducerFreqs * 1e-6, freqResponse)
+        for spectrum in spectra:
+            axes[1].plot(freqs * 1e-6, spectrum)
+        axes[1].set_title(f"Frequency Responses")
+        axes[1].legend(["transducer"] + names)
+        axes[1].set_xlim(-(fc * 6 * 1e-6), (fc * 6 * 1e-6))
+
+    return impulseResponse, freqResponse
+
+
+def task1_combined(timeVec,
+                   f0,
+                   transducerImpulseResponse,
+                   freqs,
+                   transducerFrequencyResponse,
+                   signals: list,
+                   spectra,
+                   names: list,
+                   plot=False):
+
+    combinedSignals = []
+    combinedSpectra = []
+
+    for sig, spectrum in zip(signals, spectra):
+        convolvedSignal = np.convolve(sig, transducerImpulseResponse, "same")
+        convolvedSignal /= np.max(convolvedSignal)
+        combinedSignals.append(convolvedSignal)
+
+        # spectrum, _ = powerSpectrum(convolvedSignal, fs)
+        convolvedSpectrum = transducerFrequencyResponse * spectrum
+        combinedSpectra.append(convolvedSpectrum / np.max(convolvedSpectrum))
+
+    if plot:
+        fig, axes = plt.subplots(nrows=len(signals), ncols=2, figsize=(60, 40))
+
+        for i, rSig, cSig, name in zip(range(len(signals)), signals,
+                                       combinedSignals, names):
+            axes[i][0].plot(timeVec * 1e6, rSig)
+            axes[i][0].plot(timeVec * 1e6, cSig)
+            axes[i][0].set_title(f"Convolving {name} with transducer")
+            axes[i][0].legend(
+                [f"Raw {name}", f"{name} convolved with transducer"])
+            axes[i][0].set_xticks(
+                np.linspace(timeVec[0] * 1e6, timeVec[-1] * 1e6, 11))
+
+        for i, rSpectrum, cSpectrum, name in zip(range(len(combinedSpectra)),
+                                                 spectra, combinedSignals,
+                                                 names):
+            axes[i][1].plot(freqs * 1e-6, rSpectrum / np.max(rSpectrum))
+            axes[i][1].plot(freqs * 1e-6, cSpectrum)
+            axes[i][1].set_title(
+                f"Spectrum of {name} with and without transducer influence")
+            axes[i][1].legend(
+                [f"Raw {name}", f"{name} convolved with transducer"])
+            minFreq, maxFreq = -f0 * 6 * 1e-6, f0 * 6 * 1e-6
+            axes[i][1].set_xlim((minFreq, maxFreq))
+            axes[i][1].set_xticks(np.arange(minFreq, maxFreq, 2))
+
+    return combinedSignals, combinedSpectra
+
+
+def task1_different_frequency(timeVec, freqs, originalSignals, originalSpectra,
+                              fasterSignals, fasterSpectra):
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(60, 40))
-
-    axes[0].plot(timeVec * 1e6, impulseResponse)
-    for sig in signals:
-        axes[0].plot(timeVec * 1e6, sig)
-    axes[0].set_title(f"Signals")
-    axes[0].legend(["transducer"] + names)
-    axes[0].set_xlim(-T * 1e6 / 4, T * 1e6 / 4)
-
-    axes[1].plot(transducerFreqs * 1e-6, freqResponse)
-    for spectrum in spectra:
-        axes[1].plot(freqs * 1e-6, spectrum)
-    axes[1].set_title(f"Frequency Responses")
-    axes[1].legend(["transducer"] + names)
-    axes[1].set_xlim(-(f0 * 5 * 1e-6), (f0 * 5 * 1e-6))
-
-    return timeVec, impulseResponse, freqResponse
+    for sig, fSig in zip(originalSignals, fasterSignals):
+        ...
+    ...
 
 
 def task1():
-    time, gPulse, sPulse, fftFreqs, gSpectrum, sSpectrum = task1_transmission()
-    transducerTime, transducerImpulse, transducerFrequency = task1_transducer(
-        time, [gPulse, sPulse], fftFreqs, [gSpectrum, sSpectrum],
-        ["Gauss Pulse", "Square Pulse"])
+    timeVec = np.linspace(-T / 2, T / 2, int(T * fs))
 
-    plt.show()
+    gPulse, sPulse, fftFreqs, gSpectrum, sSpectrum = task1_transmission(
+        timeVec, f0, plot=True)
+
+    # transducerImpulse, transducerFrequency = task1_transducer(
+    #     f0,
+    #     timeVec, [gPulse, sPulse],
+    #     fftFreqs, [gSpectrum, sSpectrum], ["Gauss Pulse", "Square Pulse"],
+    #     plot=False)
+
+    # convolvedSignals, convolvedSpectra = task1_combined(
+    #     timeVec,
+    #     f0,
+    #     transducerImpulse,
+    #     fftFreqs,
+    #     transducerFrequency, [gPulse, sPulse], [gSpectrum, sSpectrum],
+    #     ["Gauss Pulse", "Square Pulse"],
+    #     plot=False)
+
+    # fastGPulse, fastSPulse, _, _, _ = task1_transmission(f0, 4 * 1e6)
+    # fastConvolvedSignals, fastConvolvedSpectra = task1_combined(
+    #     timeVec,
+    #     f0,
+    #     transducerImpulse,
+    #     fftFreqs,
+    #     transducerFrequency, [fastGPulse, fastSPulse],
+    #     ["Higher freq Gauss Pulse", "Higher freq Square Pulse"],
+    #     plot=False)
+
+    # task1_different_frequency(timeVec, fftFreqs, convolvedSignals,
+    #                           convolvedSpectra, fastConvolvedSignals,
+    #                           fastConvolvedSpectra)
+
+    # plt.show()
+
+def pulseEchoResponse(r, t):
+    # G(r, t) = (sigma * (t - r/c)) / (4 * pi * r))
+    # c = 1540m/s
+    # numerator represents an impulse at time t=r/c, when scatterer is met
+    # denominator represents amplitude dimishing with distance
+    # To model two-way response: square signal
+    sigma = 1
+    c = 1540
+    numerator = sigma * (t - r/c)
+    denominator = 4 * np.pi * r
+    return (numerator / denominator)**2
+
+def depthAxis(timeVec: np.ndarray, scattererPositions: np.ndarray):
+    depth = np.zeros((len(timeVec), len(scattererPositions)))
+    for i, t in enumerate(timeVec):
+        for j, pos in enumerate(scattererPositions):
+            depth[i][j] = pulseEchoResponse(pos, t)
+    return depth    
+
+def task2(timeVec, transmittedPulse):
+    scattererPositionsCm = np.array([1, 3, 5, 7, 9, 11, 13])
+    depth = depthAxis(timeVec, scattererPositionsCm * 1e-2)
+
 
 
 def main():
     print("Exercise 2")
     task1()
+    # task2()
 
 
 if __name__ == '__main__':
